30f18ba8438249226510c6a3cf9b18ac5d90ce59
424425549@qq.com <424425549@qq.com>
2016-12-07 16:58:46 +0800

    任务： 时间： 姓名：陈旭锋

---
+++ pod测试/pod测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  pod测试
+//
+//  Created by 啦啦啦 on 2016/12/7.
+//  Copyright © 2016年 啦啦啦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ pod测试/pod测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pod测试
+//
+//  Created by 啦啦啦 on 2016/12/7.
+//  Copyright © 2016年 啦啦啦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
